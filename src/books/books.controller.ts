91200d140d126b20bd4c6ee75aeafd5a6d296d65
vrushabhdarekar <vrushabhdarekar45@gmail.com>
2026-02-10 11:45:28 +0530

    feat: add books controller with REST endpoints

---
+++ src/books/books.controller.ts
@@ -0,0 +1,31 @@
+import {Controller,Get,Post,Body,Param,Query,Patch,Delete,ParseIntPipe,} from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDto } from './dto/create-book.dto';
+
+@Controller('books')//it is just like base route books/
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Get()
+  findAll(@Query('genre') genre?: string) {
+    return this.booksService.findAll(genre);
+  }
+
+  @Post()
+  create(@Body() createBookDto: CreateBookDto) {
+    return this.booksService.create(createBookDto);
+  }
+
+  @Patch(':id')
+  updateRating(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('rating') rating: number,
+  ) {
+    return this.booksService.updateRating(id, rating);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.booksService.remove(id);
+  }
+}
